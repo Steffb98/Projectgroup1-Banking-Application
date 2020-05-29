53228dacd93515c5751c9ca2ffa6c533b5c83bd4
Steffb98 <steffburgering@hotmail.com>
2020-05-29 20:35:10 +0200

    some connectionfixes

--- Banking-Application/src/main/java/io/swagger/api/UsersApiController.java
+++ Banking-Application/src/main/java/io/swagger/api/UsersApiController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-05-21T13:09:59.263Z[GMT]")
+@CrossOrigin(origins = {"http://localhost"})
 @Controller
 public class UsersApiController implements UsersApi {
 
